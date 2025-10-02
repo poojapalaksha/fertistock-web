444580524e6eee30991defef9bc379f1f7ba78f9
poojapalaksha <poojapalaksha30@gmail.com>
2025-10-02 12:18:12 +0530

    Update: Refactored and modified admin/auth/user routes in backend

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
-const User = require('../models/User');
+const User = require('../models/user');
 
 // MongoDB connection
 mongoose.connect('mongodb://localhost:27017/fertistock', {
